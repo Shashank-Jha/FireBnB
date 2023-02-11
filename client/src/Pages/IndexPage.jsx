26a2539915723f9b89a8ce969fd284356a127677
Shashank-Jha <53325972+Shashank-Jha@users.noreply.github.com>
2023-02-11 16:59:58 +0530

    Feat : Layout created & improved file structure

---
+++ client/src/Pages/IndexPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function IndexPage() {
+  return (
+   <>Welcome to Index Page</>
+  )
+}
+
+export default IndexPage
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <div>
+      <header className="flex justify-between">
+        <a href="" className="flex gap-1 items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-8 h-8 text-primary "
+          >
+            <path
+              fillRule="evenodd"
+              d="M12.963 2.286a.75.75 0 00-1.071-.136 9.742 9.742 0 00-3.539 6.177A7.547 7.547 0 016.648 6.61a.75.75 0 00-1.152-.082A9 9 0 1015.68 4.534a7.46 7.46 0 01-2.717-2.248zM15.75 14.25a3.75 3.75 0 11-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 011.925-3.545 3.75 3.75 0 013.255 3.717z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <span className="font-bold text-xl">FireBnB</span>
+        </a>
+        <div className="border gap-2 py-2 px-4 text-sm border-gray-300 flex rounded-full items-center shadow-md shadow-gray-300">
+          <div className="font-bold">AnyWhere</div>
+          <div className="border-l border-gray-300 pl-2 font-bold">
+            Any week
+          </div>
+          <div className="border-l border-gray-300 pl-2">Add guest</div>
+          <button className="bg-primary text-white p-2 rounded-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-4 h-4"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+        <Link to="/Login" className="border gap-2 py-2 px-4 text-sm border-gray-300 flex rounded-full items-center shadow-md shadow-gray-300">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          <div className="flex items-center bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6 relative top-1 "
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+        </Link>
+      </header>
+    </div>
+  )
+}
+
+export default Header

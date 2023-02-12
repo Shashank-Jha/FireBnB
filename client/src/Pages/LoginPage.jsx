920bf06656f34c32c9f5320b04e85bbbe1d682a0
Shashank-Jha <53325972+Shashank-Jha@users.noreply.github.com>
2023-02-12 19:30:11 +0530

    Feat : Login Page UI

--- client/src/Pages/LoginPage.jsx
+++ client/src/Pages/LoginPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
   return (
-    <div>Welcome to the login page.</div>
-  )
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-48">
+        <h1 className="text-4xl font-black text-center text-primary my-2">
+          Login
+        </h1>
+        <form className="max-w-md mx-auto">
+          <input type="email" placeholder="your@email.com" />
+          <input type="password" placeholder="password" />
+          <button className="primary">Login</button>
+          <div className="py-2 text-center text-gray-500">
+            Don't have account yet?{" "}
+            <Link to={"/register"} className="font-bold text-primary underline">
+              Register Now
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;

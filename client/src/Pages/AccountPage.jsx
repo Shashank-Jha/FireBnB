1b892b5a55698f77e3d80ea2aebb80d5a84f34e1
Shashank-Jha <53325972+Shashank-Jha@users.noreply.github.com>
2023-05-30 18:58:10 +0530

    Feat : "client" Account Page

---
+++ client/src/Pages/AccountPage.jsx
@@ -0,0 +1,75 @@
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { Link, Navigate, redirect, useParams } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+
+function AccountPage() {
+  const [redirect, setRedirect] = useState(null);
+  const { ready, user, setUser } = useContext(UserContext);
+  let { subpage } = useParams();
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
+
+  if (!ready) {
+    return `Loading....`;
+  }
+
+  if (ready && !user && !redirect) {
+    return <Navigate to={"/login"} />;
+  }
+
+  function linkClass(type = null) {
+    let className = " py-2 px-6 ";
+    if (type === subpage) {
+      className += " bg-primary rounded-full text-white ";
+    }
+    return className;
+  }
+
+  async function logout(){
+    await axios.post('/logout');
+    setUser(null);
+    setRedirect('/');
+  }
+
+  if(redirect){
+    return <Navigate to={'/'}/>
+  }
+
+  return (
+    <div>
+    <nav className="w-full flex justify-center mt-8 gap-2">
+      <Link className={linkClass("profile")} to={"/account"}>
+        My Profile
+      </Link>
+      <Link className={linkClass("bookings")} to={"/account/bookings"}>
+        My Bookings
+      </Link>
+      <Link className={linkClass("places")} to={"/account/places"}>
+        My accomodations
+      </Link>
+    </nav>
+    {
+        subpage === 'profile' && (
+            <div className="text-center max-w-lg mx-auto">
+            Logged in as {user.name} ({user.email})<br/>
+            <button className="primary max-w-sm mt-2" onClick={logout}>Logout</button>
+            </div>
+        )
+    }
+    {
+      subpage === 'bookings' && (
+        <h1>My Bookings</h1>
+      )
+    }
+    {
+      subpage === 'places' && (
+        <h1>My Places</h1>
+      )
+    }
+    </div>
+  );
+}
+
+export default AccountPage;
